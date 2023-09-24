384d0d2a82f8c392e908c1acf844fcd3fca90b61
SaiPoojitha02 <143846108+SaiPoojitha02@users.noreply.github.com>
2023-09-24 00:28:52 -0400

    Added Angular

---
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js/auto';
+@Component({
+  selector: 'pb-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent implements OnInit {
+
+  public dataSource = {
+    datasets: [
+        {
+            data: [] as any[],
+            backgroundColor: [
+                '#ffcd56',
+                '#ff6384',
+                '#36a2eb',
+                '#fd6b19',
+            ]
+        }
+    ],
+    labels: [] as any[]
+};
+
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('http://localhost:3030/budget')
+    .subscribe((res: any) => {
+
+      for (var i = 0; i < res.myBudget.length; i++) {
+        this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
+        this.dataSource.labels[i] = res.myBudget[i].title;
+    }
+    this.createChart();
+
+    });
+  }
+
+  createChart() {
+    console.log("axiosdata:" + this.dataSource);
+    var ctx = document.getElementById('myChart') as HTMLCanvasElement | null;
+
+    if (!ctx) {
+        console.error("Canvas element not found!");
+        return;
+    }
+
+    var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: this.dataSource
+    });
+}
+
+}
+
+
+
+
